bbbce9cef29325a37ef81b417d2326e0e5f42e77
Marek Porwisz <marek.porwisz@nordicsemi.no>
2021-12-10 11:15:11 +0100

    [build] bump submodules and update configs
    USB support in ncs was changed and requires differen config and build
    parameters
    
    Signed-off-by: Marek Porwisz <marek.porwisz@nordicsemi.no>

--- script/otbr-setup.bash
+++ script/otbr-setup.bash
@@ -101,8 +101,6 @@ if [ "${REFERENCE_PLATFORM?}" = "ncs" ]; then
 
   # add calling of link_dongle.py script at startup to update symlink to the dongle
   sed -i '/exit 0/d' /etc/rc.local
-  grep -qxF 'sudo chmod a+x /home/pi/repo/script/link_dongle.py' /etc/rc.local || echo 'sudo chmod a+x /home/pi/repo/script/link_dongle.py' >>/etc/rc.local
-  grep -qxF 'sudo /home/pi/repo/script/link_dongle.py' /etc/rc.local || echo 'sudo /home/pi/repo/script/link_dongle.py' >>/etc/rc.local
   grep -qxF 'sudo systemctl restart otbr-agent.service' /etc/rc.local || echo 'sudo systemctl restart otbr-agent.service' >>/etc/rc.local
   echo 'exit 0' >>/etc/rc.local
 
